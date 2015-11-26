6f5408549597594a8989548c3364024077935ad3
Diego Montúfar <diogonal0@gmail.com>
2015-11-26 09:41:24 -0500

    minimal changes to corousel

--- data/data.js
+++ data/data.js
@@ -3,7 +3,7 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'mysqlPASS',
+  password : 'mysqlroot',
   database : 'handytec-web'
 });
 
